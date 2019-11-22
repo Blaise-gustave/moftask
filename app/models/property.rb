2e6d59666f9273df88f6096efa79c1f041911c28
Blaise-gustave <uyisengablaise@gmail.com>
2019-11-22 16:55:09 +0200

    --ammend

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,6 +2,6 @@ class Property < ApplicationRecord
     validates :name, presence: true
     validates :rent, presence: true
     validates :address, presence: true
-    validates :buildingAge, presence: true
+validates :BuildingAge, presence: true
     validates :remarks, presence: true
 end
